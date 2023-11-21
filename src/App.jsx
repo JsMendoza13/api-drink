12bf90952fdf3921f93a6dd142ca7b1d08d1a220
Jesus Mendoza Martinez <jsmendoza1312@gmail.com>
2023-11-21 11:56:49 -0500

    change style

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,42 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { ContextCocktail } from "./components/ContextCocktail";
 
 function App() {
   const [userInput, setUserInput] = useState("");
   const [searchResult, setSearchResult] = useState("");
+  const [contextCocktails, setContextCocktails] = useState([]);
   const url = "https://thecocktaildb.com/api/json/v1/1/search.php?s=";
 
   const handleSearch = () => {
     if (userInput.trim() === "") {
       setSearchResult(
-        <h3 className="msg"> The input field cannot be empty</h3>
+        <h3 className="msg">El campo de búsqueda no puede estar vacío</h3>
       );
     } else {
       fetch(url + userInput)
         .then((response) => response.json())
         .then((data) => {
           console.log(data);
+          // Puedes manejar los resultados de la API aquí y actualizar setSearchResult
         })
         .catch((error) => console.error("Error en la solicitud:", error));
     }
   };
+
+  useEffect(() => {
+    // Coloca aquí la lógica para obtener datos y crear los cócteles
+    const generateContextCocktails = () => {
+      const numberOfContextCocktails = 10;
+      return Array.from({ length: numberOfContextCocktails }, (_, index) => (
+        <div key={index} className="div__contextCocktail">
+          <ContextCocktail />
+        </div>
+      ));
+    };
+
+    const newContextCocktails = generateContextCocktails();
+    setContextCocktails(newContextCocktails);
+  }, []); // El array vacío como segundo argumento asegura que este efecto solo se ejecute una vez al montar el componente
 
   return (
     <>
@@ -42,36 +59,7 @@ function App() {
       </header>
       <section className="section__cocktail">
         <div id="result">{searchResult}</div>
-        <div className="div__contextCocktail">
-          <ContextCocktail />
-        </div>
-        <div className="div__contextCocktail">
-          <ContextCocktail />
-        </div>
-        <div className="div__contextCocktail">
-          <ContextCocktail />
-        </div>
-        <div className="div__contextCocktail">
-          <ContextCocktail />
-        </div>
-        <div className="div__contextCocktail">
-          <ContextCocktail />
-        </div>
-        <div className="div__contextCocktail">
-          <ContextCocktail />
-        </div>
-        <div className="div__contextCocktail">
-          <ContextCocktail />
-        </div>
-        <div className="div__contextCocktail">
-          <ContextCocktail />
-        </div>
-        <div className="div__contextCocktail">
-          <ContextCocktail />
-        </div>
-        <div className="div__contextCocktail">
-          <ContextCocktail />
-        </div>
+        {contextCocktails}
       </section>
     </>
   );
